a469a6f5faa0cf055391c6ff10adb38e4fb0c643
Nils Brinkmann <monsdar+github@gmail.com>
2019-03-05 20:51:44 +0100

    Fixed: Made the conanfile compatible with current Conan versions

--- conanfile.py
+++ conanfile.py
@@ -9,24 +9,21 @@ class Sqlite3ccConan(ConanFile):
     generators = "cmake"
     exports = "*"
 
-    def config(self):
-        self.requires.add("SQLite3/3.12.1@monsdar/testing")
-        self.requires.add("Boost/1.60.0@lasote/stable")
+    def requirements(self):
+        self.requires.add("sqlite3/3.27.2@bincrafters/stable")
+        self.requires.add("boost/1.69.0@conan/stable")
     
-    def source(self):
-        #this packet brings all necessary sources with it
-        pass
-
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.install()
 
     def package(self):
-        self.copy("*.h", dst="include", src="%s/sqlite3cc-0.1.1/include" % self.conanfile_directory)
+        self.copy("*.h", dst="include", src="%s/sqlite3cc-0.1.1/include" % self.source_folder)
         self.copy("*.lib", dst="lib", src="lib")
         self.copy("*.a", dst="lib", src="lib")
 
     def package_info(self):
-        self.cpp_info.libs = ["sqlite3cc-cmake"]
+        self.cpp_info.libs = ["sqlite3cc"]
         
